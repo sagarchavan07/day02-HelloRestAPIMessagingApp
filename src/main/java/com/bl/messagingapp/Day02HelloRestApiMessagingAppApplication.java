8e4364419d4cc7e08059f30f9953e0d83e38f668
sagarchavan07 <chavansagar96044@gmail.com>
2022-09-01 12:38:02 +0530

    initial commit

---
+++ src/main/java/com/bl/messagingapp/Day02HelloRestApiMessagingAppApplication.java
@@ -0,0 +1,13 @@
+package com.bl.messagingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day02HelloRestApiMessagingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day02HelloRestApiMessagingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bl/messagingapp/Day02HelloRestApiMessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bl.messagingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day02HelloRestApiMessagingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
